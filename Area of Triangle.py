bd5ad4360bbdd28f8352e283b26124b43caec53e
kranthi kumar <krkranthikumar2003@gmail.com>
2025-09-25 12:13:10 +0530

    Add files via upload

---
+++ Area of Triangle.py
@@ -0,0 +1,6 @@
+#Take the input values from the user
+base = int(input('enter the length of the base of the triangle: '))
+height = int(input('enter the height of the triangle: '))
+#calculate the area of triangle
+area = 0.5 * base * height
+print(f'Area of the Triangle: {area}')
