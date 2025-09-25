d8e72ba9a6762f47b7692c4d125d1614d3251eec
kranthi kumar <krkranthikumar2003@gmail.com>
2025-09-25 12:03:19 +0530

    Add files via upload

---
+++ arthmetic.py
@@ -0,0 +1,29 @@
+#Addition
+num1 = int(input('enter the number: '))
+num2 = int(input('enter the number: '))
+summ = num1+num2
+print(f"sum_result:{summ}")
+
+#Substraction
+num3 = int(input('enter the number: '))
+num4 = int(input('enter the number: '))
+sub = num3-num4
+print(f"sub_result:{sub}")
+
+#Multiplication
+num5 = int(input('enter the number: '))
+num6 = int(input('enter the number: '))
+mul = num5*num6
+print(f"mul_result:{,ul}")
+
+#Division
+num7 = int(input('enter the number: '))
+num8 = int(input('enter the number: '))
+if num8 ==0:
+    print('Division by zero is not allowed')
+else:
+    div = num7/num8
+    print(f"div_result:{div}")
+
+
+         
